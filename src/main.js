082cc16d0e370aeb0bb7e19631627aba902db87b
mxwiser <neoway@126.com>
2023-03-29 18:06:00 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+
+
 import './assets/main.css'
+// import './assets/font.css'
+
 
 //google webfontloader
-import { loadFonts } from './plugins/webfontloader'
-loadFonts()
+// import { loadFonts } from './plugins/webfontloader'
+// loadFonts()
 
 createApp(App)
   .use(router)
--- src/plugins/webfontloader.js
+++ src/plugins/webfontloader.js
@@ -8,13 +8,13 @@ export async function loadFonts () {
   const webFontLoader = await import(/* webpackChunkName: "webfontloader" */'webfontloader')
 
   webFontLoader.load({
-    // google: {
-    //   families: ['Roboto:100,300,400,500,700,900&display=swap'],
-    // },
-    custom: {
-      families: ['alis','sans-serif'],
-      urls: ['../assets/fonts.css']
+    google: {
+      families: ['Roboto:100,300,400,500,700,900&display=swap'],
     },
+    // custom: {
+    //   families: [],
+    //
+    // },
 
   })
 }
