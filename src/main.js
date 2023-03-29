ec8a08e320a340728cc8996c4ce3b94753f3395a
mxwiser <neoway@126.com>
2023-03-29 18:32:52 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import './assets/main.css'
-import './assets/font.css'
+
 
 
 //google webfontloader
+import './assets/font.css'
 import { loadFonts } from './plugins/webfontloader'
 loadFonts()
 
