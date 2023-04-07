155bd40b3b2822aeb5c47c993d8bc0776ddb22c4
mxwiser <neoway@126.com>
2023-04-07 18:20:13 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -19,5 +19,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-    base:'/info'
+    base:'/info',
+    server: {
+        host:'dgims.tech',
+        port:80
+    }
 })
